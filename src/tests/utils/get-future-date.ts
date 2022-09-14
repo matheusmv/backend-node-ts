8d3eb4796fc946c8517399df85d2d224404c932e
Matheus Matias <matheus.matias.viana@hotmail.com>
2022-09-13 21:29:15 -0300

    Update get-future-date.ts

--- src/tests/utils/get-future-date.ts
+++ src/tests/utils/get-future-date.ts
@@ -3,7 +3,7 @@ import { parseISO, setYear } from 'date-fns';
 /**
  * Returns a date one year ahead
  *
- * @param date date string in format "yyyy-mm-dd"
+ * @param date string in format "yyyy-mm-dd"
  */
 export function getFutureDate(date: string): Date {
   return setYear(parseISO(date), new Date().getFullYear() + 1);
